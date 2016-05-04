3471419918b31f068368eb23473788f8d8115407
esoyke <soykes@yahoo.com>
2016-05-04 12:32:47 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name trainApp
+ * @description
+ * # trainApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('trainApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.bootstrap'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'LeaderboardCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
